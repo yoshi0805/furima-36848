ce6a935ec2657443760a4717824fbc0e78313b1e
yoshi0805 <90741509+yoshi0805@users.noreply.github.com>
2021-11-30 11:17:26 +0900

    itemモデル　バリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,18 @@
 class Item < ApplicationRecord
   belongs_to :user
 
+  with_options presence: true do
+  validates :item_name
+  validates :detail
+  validates :category_id
+  validates :status_id
+  validates :shipping_burden_id
+  validates :prefecture_id
+  validates :delivery_day_id
+  validates :image
+  validates :price,inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
+  end
+
+  validates :price, numericality: true 
+  
 end
