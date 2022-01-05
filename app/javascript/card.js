e35ddc6b581b8f305465f4a075077b256e2df15f
yoshi0805 <90741509+yoshi0805@users.noreply.github.com>
2022-01-05 11:54:39 +0900

    環境設定（公開キー）

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = ()=> {
-  Payjp.setPublicKey("pk_test_caf7013ed38543f988c7e703")
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
