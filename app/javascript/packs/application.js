47cb3402f80ddfd62e104b036e1d38ffb8199fe1
yoshi0805 <90741509+yoshi0805@users.noreply.github.com>
2022-01-07 11:50:54 +0900

    jsネーム変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../price")
-require("../card")
+require("../order")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
